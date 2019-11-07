15cf10700adc57ca4e4d6d06df72e6c2a59d76f4
Joe <joee3135@gmail.com>
2019-11-06 20:42:50 -0600

    fixing errors

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,11 @@
 class Api::ProductsController < ApplicationController
+  def index
+    @products = Product.all
+    render 'products.json.jb'
+  end
+
+  def first
+    @product = Product.find_by(id: 1)
+    render 'one_product.json.jb'
+  end
 end
